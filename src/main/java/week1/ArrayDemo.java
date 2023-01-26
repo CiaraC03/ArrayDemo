f572fd516b987c2e44b8d8921ad59e63d07e7a15
G00402357 <G00402357@atu.ie>
2023-01-26 12:48:23 +0000

    New array List added

--- src/main/java/week1/ArrayDemo.java
+++ src/main/java/week1/ArrayDemo.java
@@ -1,10 +1,30 @@
 package week1;
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayDemo {
+
     public static void main(String[] args) {
+        ArrayList<Integer> myList = new ArrayList<>();
         int[] firstArray;
+
+        myList.add(100);
+        myList.add(200);
+        myList.add(300);
+        myList.add(400);
+        myList.add(500);
+        myList.add(600);
+        myList.add(700);
+        myList.add(800);
+        myList.add(900);
+        myList.add(1000);
+
+        //arrayList
+        for(int i : myList){
+            System.out.println(i);
+        }
 
         firstArray = new int[10];
 
@@ -18,6 +38,7 @@ public class ArrayDemo {
         firstArray[7] = 800;
         firstArray[8] = 900;
         firstArray[9] = 1000;
+        loop(myList);
 
 
 
@@ -33,18 +54,31 @@ public class ArrayDemo {
         System.out.println("Element at 8: " +firstArray[8]);
         System.out.println("Element at 9: " +firstArray[9]);
 
+
         //user is prompted for a new value
         Scanner sc = new Scanner(System.in);
         try {
             System.out.println("Please enter a value: ");
-            firstArray[10] = sc.nextInt();
+            myList.add(sc.nextInt());
+            loop(myList);
+
         } catch(ArrayIndexOutOfBoundsException e)
         {
-            System.out.print("Too big value");
+            System.out.print("Too big value of memory block" + e);
         }
 
 
 
+
+    }
+    public static void loop(ArrayList<Integer> someArrayList)
+    {
+        int indexValue = 0;
+        for(int value: someArrayList)
+        {
+            System.out.println("Element at index" + indexValue + " : " + value);
+            indexValue++;
+        }
 
     }
 }
