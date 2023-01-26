863d76496ff714a8190b05a2c3171c7f83d1fb7a
G00402357 <G00402357@atu.ie>
2023-01-26 11:33:58 +0000

    New code added

---
+++ src/main/java/week1/ArrayDemo.java
@@ -0,0 +1,33 @@
+package week1;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+        int[] firstArray;
+
+        firstArray = new int[10];
+
+        firstArray[0] = 100;
+        firstArray[1] = 200;
+        firstArray[2] = 300;
+        firstArray[3] = 400;
+        firstArray[4] = 500;
+        firstArray[5] = 600;
+        firstArray[6] = 700;
+        firstArray[7] = 800;
+        firstArray[8] = 900;
+        firstArray[9] = 1000;
+
+        //prints
+        System.out.println("Element at 0: " +firstArray[0]);
+        System.out.println("Element at 1: " +firstArray[1]);
+        System.out.println("Element at 2: " +firstArray[2]);
+        System.out.println("Element at 3: " +firstArray[3]);
+        System.out.println("Element at 4: " +firstArray[4]);
+        System.out.println("Element at 5: " +firstArray[5]);
+        System.out.println("Element at 6: " +firstArray[6]);
+        System.out.println("Element at 7: " +firstArray[7]);
+        System.out.println("Element at 8: " +firstArray[8]);
+        System.out.println("Element at 9: " +firstArray[9]);
+
+    }
+}
