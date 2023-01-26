1647ab1022b4c9b9dff5859326ca4ac989fbdb62
G00402357 <G00402357@atu.ie>
2023-01-26 11:52:41 +0000

    New code added

--- src/main/java/week1/ArrayDemo.java
+++ src/main/java/week1/ArrayDemo.java
@@ -17,6 +17,8 @@ public class ArrayDemo {
         firstArray[8] = 900;
         firstArray[9] = 1000;
 
+
+
         //prints
         System.out.println("Element at 0: " +firstArray[0]);
         System.out.println("Element at 1: " +firstArray[1]);
@@ -28,6 +30,9 @@ public class ArrayDemo {
         System.out.println("Element at 7: " +firstArray[7]);
         System.out.println("Element at 8: " +firstArray[8]);
         System.out.println("Element at 9: " +firstArray[9]);
+
+        //user is prompted for a new value
+        System.out.println("Please enter a value: ");
 
     }
 }
